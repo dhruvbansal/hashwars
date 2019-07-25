4f90df4b4b319f963f9158ba4e91b8bd481ca4aa
Dhruv Bansal <shrub.vandal@gmail.com>
2019-07-24 22:12:33 -0500

    ehhh

--- simulations/earth_mars/many_runs.py
+++ simulations/earth_mars/many_runs.py
@@ -3,6 +3,7 @@ from pickle import dumps
 from concurrent.futures import ProcessPoolExecutor
 from random import shuffle
 
+from math import ceil
 from numpy import arange, array, concatenate
 
 from simulations.earth_mars.single_run import *
@@ -21,10 +22,7 @@ def _parse_array(spec):
     else:
         return array([float(n) for n in spec.split(',')])
 
-def _single_run(params):
-    distance, hashrate_ratio = params
-    stderr.write("Running distance={} hashrate_ratio={}\n".format(distance, hashrate_ratio))
-    stderr.flush()
+def _single_run(hours, distance, hashrate_ratio):
     (distance,
     mars_miners,
     earth_miners,
@@ -34,49 +32,81 @@ def _single_run(params):
     earth_miners_mars_blocks,
     earth_miners_earth_blocks) = single_run(hours, distance, hashrate_ratio)
     mars_blocks_ratio = mars_miners_mars_blocks[-1]/(mars_miners_mars_blocks[-1]+mars_miners_earth_blocks[-1])
-    return (distance, hashrate_ratio, mars_blocks_ratio)
+    return mars_blocks_ratio
 
-def many_runs(hours, runs_per_sample, distances, hashrate_ratios):
+def _single_population(params):
+    hours, distance, hashrate_ratio, sample_size, relative_error, max_runs = params
+    hours = int(hours)
+    distance = float(distance)
+    hashrate_ratio = float(hashrate_ratio)
+    sample_size = int(sample_size)
+    relative_error = float(relative_error)
+    max_runs = int(max_runs)
+
+    n = sample_size
+    sample = array([_single_run(hours, distance, hashrate_ratio) for sample_trial in range(int(sample_size))])
+    miu = sample.mean()
+    sigma = sample.std()
+
+    W = abs(relative_error * miu)
+    if W > 0:
+        N = ceil(16 * (pow(sigma, 2) / pow(W, 2))) # https://en.wikipedia.org/wiki/Sample_size_determination
+    else: 
+        N = 1
+    if N > max_runs:
+        N = max_runs
+    if N < sample_size:
+        N = sample_size
+    population = array([_single_run(hours, distance, hashrate_ratio) for trial in range(N)])
+    Miu = population.mean()
+    Sigma = population.std()
+    
+    stderr.write("L={} D={:0.0f} HR={:0.4f} => {} {:0.4f} ERROR {:0.4f}\n".format(
+        hours, distance, hashrate_ratio,
+        N, Miu,
+        abs(Sigma - sigma)/Sigma if Sigma != 0 else 0.0,
+    )); stderr.flush()
+    return (distance, hashrate_ratio, concatenate((population, sample)))
+    
+def many_runs(hours, sample_size, relative_error, max_runs, distances, hashrate_ratios):
     stderr.write("HOURS: {}\n".format(hours))
-    stderr.write("RUNS PER POINT: {}\n".format(runs_per_sample))
+    stderr.write("SAMPLE SIZE: {} RELATIVE ERROR: {} MAX RUNS: {}\n".format(sample_size, relative_error, max_runs))
     stderr.write("DISTANCES: {} - {} ({} total)\n".format(distances[0], distances[-1], len(distances)))
     stderr.write("HASHRATE RATIOS: {} - {} ({} total)\n".format(hashrate_ratios[0], hashrate_ratios[-1], len(hashrate_ratios)))
-    runs = []
+    stderr.flush()
+    populations = []
     for distance in distances:
         for hashrate_ratio in hashrate_ratios:
-            for run in range(runs_per_sample):
-                runs.append((distance, hashrate_ratio))
-    stderr.write("TOTAL RUNS: {}\n".format(len(runs)))
-    stderr.flush()
-    shuffle(runs)
-    stderr.write("Opening process pool executor...\n")
-    stderr.flush()
+            populations.append((hours, distance, hashrate_ratio, sample_size, relative_error, max_runs))
+    stderr.write("POPULATIONS: {}\n".format(len(populations))); stderr.flush()
+    stderr.write("Randomizing populations...\n")
+    shuffle(populations)
+    stderr.write("Opening process pool executor...\n"); stderr.flush()
+
     with ProcessPoolExecutor() as executor:
-        results = list(executor.map(_single_run, runs))
-        stderr.write("Obtained Results\n")
-        stderr.flush()
+        results = list(executor.map(_single_population, populations))
+        stderr.write("Obtained results...\n"); stderr.flush()
         mars_blocks_ratios = []
         for distance in distances:
             mars_blocks_ratios_at_distance = []
             results_at_distance = [result for result in results if result[0] == distance]
             for hashrate_ratio in hashrate_ratios:
-                mars_blocks_ratios_at_distance_and_hashrate_ratio = []
-                results_at_distance_and_hashrate_ratio = [result for result in results_at_distance if result[1] == hashrate_ratio]
-                mars_blocks_ratios_at_distance_and_hashrate_ratio = array([result[2] for result in results_at_distance_and_hashrate_ratio])
+                mars_blocks_ratios_at_distance_and_hashrate_ratio = [result for result in results_at_distance if result[1] == hashrate_ratio][0][2]
                 mars_blocks_ratios_at_distance.append(mars_blocks_ratios_at_distance_and_hashrate_ratio)
             mars_blocks_ratios.append(mars_blocks_ratios_at_distance)
-        return (distances, hashrate_ratios, array(mars_blocks_ratios))
+        stderr.write("Collated results...\n"); stderr.flush()
+        return (distances, hashrate_ratios, mars_blocks_ratios)
 
 if __name__ == '__main__':
-    if len(argv) < 5:
-        stderr.write("usage: many_runs.py HOURS RUNS_PER_SAMPLE DISTANCES HASHRATE_RATIOS [OUTPUT_PATH]\n")
+    if len(argv) < 7:
+        stderr.write("usage: many_runs.py HOURS SAMPLE_SIZE RELATIVE_ERROR MAX_RUNS DISTANCES HASHRATE_RATIOS [OUTPUT_PATH]\n")
         exit(1)
-    hours, runs_per_sample, distances, hashrate_ratios = argv[1:5]
-    results = many_runs(int(hours), int(runs_per_sample), _parse_array(distances), _parse_array(hashrate_ratios))
+    hours, sample_size, relative_error, max_runs, distances, hashrate_ratios = argv[1:7]
+    results = many_runs(int(hours), int(sample_size), float(relative_error), int(max_runs), _parse_array(distances), _parse_array(hashrate_ratios))
     output = dumps(results)
 
-    if len(argv) > 5:
-        with open(argv[5], 'wb') as f:
+    if len(argv) > 7:
+        with open(argv[7], 'wb') as f:
             f.write(output)
     else:
         stdout.buffer.write(output)
--- simulations/earth_mars/plot_many_runs_contour.py
+++ simulations/earth_mars/plot_many_runs_contour.py
@@ -3,41 +3,65 @@ from sys import argv, stderr, stdout, stdin
 from pickle import loads
 
 import matplotlib.pyplot as plt
-from numpy import array, mean, var
+from numpy import array, mean, std
 
 from simulations.earth_mars.single_run import *
 
+def _show_populations(ax, population_distances, population_hashrate_ratios):
+    show_populations = environ.get("POPULATIONS")
+    if show_populations:
+        ax.scatter(population_distances, population_hashrate_ratios, s=5, color='black', linewidths=0.01, alpha=0.25)
+
 def plot_contour(distances, hashrate_ratios, mars_blocks_ratios):
 
-    show_samples = environ.get("SAMPLES")
-
-    mars_blocks_ratios_means = mean(mars_blocks_ratios, axis=2)
-    mars_blocks_ratios_vars = var(mars_blocks_ratios, axis=2)
-
-    fig, (ax_means, ax_vars) = plt.subplots(nrows=2, sharex=True)
+    fig, (ax_means, ax_counts, ax_std_devs) = plt.subplots(nrows=3, sharex=True)
     fig.suptitle('Fraction Blocks Mined on Mars')
 
-    sample_distances = []
-    sample_hashrate_ratios = []
+    population_distances = []
+    population_hashrate_ratios = []
     for distance in distances:
         for hashrate_ratio in hashrate_ratios:
-            sample_distances.append(distance)
-            sample_hashrate_ratios.append(hashrate_ratio)
+            population_distances.append(distance)
+            population_hashrate_ratios.append(hashrate_ratio)
+
+    mars_blocks_ratios_counts = []
+    mars_blocks_ratios_means = []
+    mars_blocks_ratios_std_devs = []
+    for distance_index, distance in enumerate(distances):
+        mars_blocks_ratios_counts_at_distance = []
+        mars_blocks_ratios_means_at_distance = []
+        mars_blocks_ratios_std_devs_at_distance = []
+        for hashrate_ratio_index, hashrate_ratio in enumerate(hashrate_ratios):
+            mars_blocks_ratios_at_distance_and_hashrate = mars_blocks_ratios[distance_index][hashrate_ratio_index]
+            mars_blocks_ratios_counts_at_distance.append(len(mars_blocks_ratios_at_distance_and_hashrate))
+            mars_blocks_ratios_means_at_distance.append(mars_blocks_ratios_at_distance_and_hashrate.mean())
+            mars_blocks_ratios_std_devs_at_distance.append(mars_blocks_ratios_at_distance_and_hashrate.std())
+        mars_blocks_ratios_counts.append(mars_blocks_ratios_counts_at_distance)
+        mars_blocks_ratios_means.append(mars_blocks_ratios_means_at_distance)
+        mars_blocks_ratios_std_devs.append(mars_blocks_ratios_std_devs_at_distance)
+    mars_blocks_ratios_counts = array(mars_blocks_ratios_counts)
+    mars_blocks_ratios_means = array(mars_blocks_ratios_means)
+    mars_blocks_ratios_std_devs = array(mars_blocks_ratios_std_devs)
 
     ax_means.set_title("Mean")
     ax_means.set_ylabel('Earth/Mars Hashrate')
     means_mappable = ax_means.contourf(distances, hashrate_ratios, mars_blocks_ratios_means.transpose(), cmap="coolwarm")
     plt.colorbar(means_mappable, ax=ax_means)
-    if show_samples:
-        ax_means.scatter(sample_distances, sample_hashrate_ratios, s=5, color='black', linewidths=0.01, alpha=0.25)
+    _show_populations(ax_means, population_distances, population_hashrate_ratios)
 
-    ax_vars.set_title("Variance")
-    ax_vars.set_ylabel('Earth/Mars Hashrate')
-    ax_vars.set_xlabel("Seconds")
-    vars_mappable = ax_vars.contourf(distances, hashrate_ratios, mars_blocks_ratios_vars.transpose(), cmap="Greys")
-    plt.colorbar(vars_mappable, ax=ax_vars)
-    if show_samples:
-        ax_vars.scatter(sample_distances, sample_hashrate_ratios, s=5, color='black', linewidths=0.01, alpha=0.25)
+    ax_counts.set_title("Counts")
+    ax_counts.set_ylabel('Earth/Mars Hashrate')
+    ax_counts.set_xlabel("Seconds")
+    counts_mappable = ax_counts.contourf(distances, hashrate_ratios, mars_blocks_ratios_counts.transpose(), cmap="Greys")
+    plt.colorbar(counts_mappable, ax=ax_counts)
+    _show_populations(ax_counts, population_distances, population_hashrate_ratios)
+
+    ax_std_devs.set_title("Std. Deviation")
+    ax_std_devs.set_ylabel('Earth/Mars Hashrate')
+    ax_std_devs.set_xlabel("Seconds")
+    std_devs_mappable = ax_std_devs.contourf(distances, hashrate_ratios, mars_blocks_ratios_std_devs.transpose(), cmap="Greys")
+    plt.colorbar(std_devs_mappable, ax=ax_std_devs)
+    _show_populations(ax_std_devs, population_distances, population_hashrate_ratios)
 
     plt.show()
 
